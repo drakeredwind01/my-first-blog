faf3df832e2af38d596230f58ac9fb14a48422a0
drake <drakeredwind01@gmail.com>
2026-03-12 17:54:48 -0700

    update

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.post_list, name='base'),
+    path('resume/', views.resume, name='resume'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,6 @@ def post_list(request):
     # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('title')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def resume(request):
+    return render(request, 'blog/resume.html')
