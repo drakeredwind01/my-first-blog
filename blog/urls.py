3e31dea5a3e5f30b444d21a6b34d6d9fe0bd5f04
drake <drakeredwind01@gmail.com>
2026-03-16 17:22:52 -0700

    completed first form and tested it

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('resume/', views.resume, name='resume'),
     path('test_zone/', views.test_zone, name='test_zone'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
 ]
