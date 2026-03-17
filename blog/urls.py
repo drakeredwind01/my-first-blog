73433dc57d8711c4fa26afe131b67e6e600d20e2
drake <drakeredwind01@gmail.com>
2026-03-16 17:38:50 -0700

    completed making first edit page

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('test_zone/', views.test_zone, name='test_zone'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
